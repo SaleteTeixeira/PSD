61b72a9ef2c55a1c66ce04aae8b60cc3cd75142f
Diogo Pimenta <a75369@alunos.uminho.pt>
2018-12-05 12:51:20 +0000

    Adicionar compilação de erlang ao script

--- exec.sh
+++ exec.sh
@@ -5,6 +5,7 @@ trap 'kill $(jobs -p)' EXIT
 ERL='/usr/lib/erlang/bin/erl -pa ./erlang/out/production/erlang -pa ./erlang -eval server:main\(\). -s init stop -noshell &'
 
 ant -f ./client -Dnb.internal.action.name=rebuild clean jar
+/usr/lib/erlang/bin/erlc -pa ./erlang/out/production/erlang -pa ./erlang +debug_info ./erlang/src/*.erl
 
 eval $ERL
 java -jar ./client/dist/client.jar
