0b8cd438d0f52c87f4342e219e5640055045c978
Diogo Pimenta <a75369@alunos.uminho.pt>
2019-01-17 20:15:16 +0000

    forwarding das ações dos users para as exchanges

--- erlang/src/server.erl
+++ erlang/src/server.erl
@@ -7,6 +7,8 @@
 -include("messages.hrl").
 
 %% Implementation
+number_of_exchanges() -> 3.
+
 main() ->
   loginManager:create(),
   loginManager:create_account("diogo", "diogo", "Investor"),
@@ -66,102 +68,121 @@ handle(Socket, Username, Role) ->
 hash_string(String) -> hash_string(String, 0).
 hash_string([], Acc) -> Acc;
 hash_string([H | T], Acc) ->
-  hash_string(T, Acc + (H + ((1-rand:uniform()) * 100))).
+  hash_string(T, Acc + (H + ((1 - rand:uniform()) * 100))).
 
 get_ip(Index) ->
-  0.
+  (Index rem number_of_exchanges()) + 12345.
 
 get_exchange(Company) ->
   get_ip(hash_string(Company)).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% COMPANY
-
-handle_company(Socket, Username) ->
+handle_company(ClientSocket, Company) ->
+  {ok, ExchangeSocket} = gen_tcp:connect("localhost", get_exchange(Company), [binary, {packet, 4}, {reuseaddr, true}]),
+  handle_company(ClientSocket, Company, ExchangeSocket).
+handle_company(ClientSocket, Company, ExchangeSocket) ->
   receive
     {tcp, _, Bin} ->
       io:fwrite('~p~n', [messages:decode_msg(Bin, 'Request')]),
       case messages:decode_msg(Bin, 'Request') of
         {'Request', "LogoutRequest"} ->
-          logout_company(Socket, Username, Bin);
+          logout_company(ClientSocket, Company, ExchangeSocket, Bin);
         {'Request', "Auction"} ->
-          company_auction(Socket, Username, Bin);
+          company_auction(ClientSocket, Company, ExchangeSocket, Bin);
         {'Request', "FixedLoan"} ->
-          company_fixed(Socket, Username, Bin);
+          company_fixed(ClientSocket, Company, ExchangeSocket, Bin);
         _ ->
           io:fwrite('WRONG REQUEST.\n'),
-          handle_company(Socket, Username)
+          handle_company(ClientSocket, Company, ExchangeSocket)
       end;
     {tcp_closed, _} ->
       io:fwrite('Company terminated in handle.~n'),
-      loginManager:logout(Username),
+      loginManager:logout(Company),
       invalid;
     {tcp_error, _, _} ->
       io:fwrite('TCP error in handle.~n'),
-      loginManager:logout(Username),
+      loginManager:logout(Company),
       invalid
   end.
 
-company_fixed(Socket, Username, Bin) ->
-  io:fwrite('Fixed loan creation request. ~p~n', [Username]),
-  case messages:decode_msg(Bin, 'FixedLoan') of
+company_fixed(ClientSocket, Company, ExchangeSocket, Bin) ->
+  io:fwrite('Fixed loan creation request. ~p~n', [Company]),
+  io:fwrite('Forwarding'),
+  gen_tcp:send(ExchangeSocket, Bin),
+  receive
+    {tcp, _, Bin} ->
+      gen_tcp:send(ClientSocket, Bin),
+      handle_company(ClientSocket, Company, ExchangeSocket);
     _ ->
-      io:fwrite('Fixed loan creation request not ok.\n'),
-      Reply = messages:encode_msg(#'Reply'{result = false, message = "Malformed fixed loan creation request"}),
-      gen_tcp:send(Socket, Reply),
-      handle_company(Socket, Username)
+      loginManager:logout(Company)
   end.
 
-company_auction(Socket, Username, Bin) ->
-  io:fwrite('Auction creation request. ~p~n', [Username]),
-  case messages:decode_msg(Bin, 'Auction') of
+company_auction(ClientSocket, Company, ExchangeSocket, Bin) ->
+  io:fwrite('Auction creation request. ~p~n', [Company]),
+  io:fwrite('Forwarding'),
+  gen_tcp:send(ExchangeSocket, Bin),
+  receive
+    {tcp, _, Bin} ->
+      gen_tcp:send(ClientSocket, Bin),
+      handle_company(ClientSocket, Company, ExchangeSocket);
     _ ->
-      io:fwrite('Auction creation request not ok.\n'),
-      Reply = messages:encode_msg(#'Reply'{result = false, message = "Malformed auction creation request"}),
-      gen_tcp:send(Socket, Reply),
-      handle_company(Socket, Username)
+      loginManager:logout(Company)
   end.
 
-logout_company(Socket, Username, Bin) ->
-  io:fwrite('Company logout request. ~p~n', [Username]),
+logout_company(Socket, Company, ExchangeSocket, Bin) ->
+  io:fwrite('Company logout request. ~p~n', [Company]),
   case messages:decode_msg(Bin, 'LogoutRequest') of
-    {_, _, Username} ->
-      case loginManager:logout(Username) of
+    {_, _, Company} ->
+      case loginManager:logout(Company) of
         ok ->
           io:fwrite('Logout ok.\n'),
           Reply = messages:encode_msg(#'Reply'{result = true, message = "Logout ok"}),
           gen_tcp:send(Socket, Reply),
+          gen_tcp:close(ExchangeSocket),
           authenticate(Socket);
         _ ->
           io:fwrite('Logout not ok.\n'),
           Reply = messages:encode_msg(#'Reply'{result = false, message = "Logout not ok"}),
           gen_tcp:send(Socket, Reply),
-          handle_company(Socket, Username)
+          handle_company(Socket, Company, ExchangeSocket)
       end;
     _ ->
       io:fwrite('Logout not ok.\n'),
       Reply = messages:encode_msg(#'Reply'{result = false, message = "Logout not ok"}),
       gen_tcp:send(Socket, Reply),
-      handle_company(Socket, Username)
+      handle_company(Socket, Company, ExchangeSocket)
   end.
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% INVESTOR
-
-handle_investor(Socket, Username) ->
+get_exchange_map(ExchangeMap, Accum) ->
+  N = number_of_exchanges() * 2,
+  case Accum of
+    N -> ExchangeMap;
+    _ ->
+      Port = get_ip(Accum),
+      {ok, Socket} = gen_tcp:connect("localhost", Port, [binary, {packet, 4}, {reuseaddr, true}]),
+      maps:put(Port, Socket, ExchangeMap),
+      get_exchange_map(ExchangeMap, Accum + 1)
+  end.
+handle_investor(ClientSocket, Username) ->
+  ExchangeMap = get_exchange_map(#{}, number_of_exchanges()),
+  handle_investor(ClientSocket, Username, ExchangeMap).
+handle_investor(ClientSocket, Username, ExchangeMap) ->
   receive
     {tcp, _, Bin} ->
       io:fwrite('~p~n', [messages:decode_msg(Bin, 'Request')]),
       case messages:decode_msg(Bin, 'Request') of
         {'Request', "LogoutRequest"} ->
-          logout_investor(Socket, Username, Bin);
+          logout_investor(ClientSocket, Username, ExchangeMap, Bin);
         {'Request', "AuctionBid"} ->
-          investor_bid(Socket, Username, Bin);
+          investor_bid(ClientSocket, Username, ExchangeMap, Bin);
         {'Request', "FixedSubscription"} ->
-          investor_fixed(Socket, Username, Bin);
+          investor_fixed(ClientSocket, Username, ExchangeMap, Bin);
         _ ->
           io:fwrite('WRONG REQUEST.\n'),
-          handle_investor(Socket, Username)
+          handle_investor(ClientSocket, Username, ExchangeMap)
       end;
     {tcp_closed, _} ->
       io:fwrite('Investor terminated in handle.~n'),
@@ -173,27 +194,37 @@ handle_investor(Socket, Username) ->
       invalid
   end.
 
-investor_fixed(Socket, Username, Bin) ->
+investor_fixed(ClientSocket, Username, ExchangeMap, Bin) ->
   io:fwrite('Fixed subscription request. ~p~n', [Username]),
-  case messages:decode_msg(Bin, 'FixedSubscription') of
+  io:fwrite('Forwarding'),
+  {_, _, _, Company, _} = messages:decode_msg(Bin, 'FixedSubscription'),
+  Port = get_exchange(Company),
+  {ok, ExchangeSocket} = maps:find(Port, ExchangeMap),
+  gen_tcp:send(ExchangeSocket, Bin),
+  receive
+    {tcp, _, Bin} ->
+      gen_tcp:send(ClientSocket, Bin),
+      handle_investor(ClientSocket, Username, ExchangeMap);
     _ ->
-      io:fwrite('Fixed subscription request not ok.\n'),
-      Reply = messages:encode_msg(#'Reply'{result = false, message = "Malformed fixed subscription request"}),
-      gen_tcp:send(Socket, Reply),
-      handle_investor(Socket, Username)
+      loginManager:logout(Username)
   end.
 
-investor_bid(Socket, Username, Bin) ->
+investor_bid(ClientSocket, Username, ExchangeMap, Bin) ->
   io:fwrite('Auction bid request. ~p~n', [Username]),
-  case messages:decode_msg(Bin, 'AuctionBid') of
+  io:fwrite('Forwarding'),
+  {_, _, _, Company, _, _} = messages:decode_msg(Bin, 'AuctionBid'),
+  Port = get_exchange(Company),
+  {ok, ExchangeSocket} = maps:find(Port, ExchangeMap),
+  gen_tcp:send(ExchangeSocket, Bin),
+  receive
+    {tcp, _, Bin} ->
+      gen_tcp:send(ClientSocket, Bin),
+      handle_investor(ClientSocket, Username, ExchangeMap);
     _ ->
-      io:fwrite('Auction bid request not ok.\n'),
-      Reply = messages:encode_msg(#'Reply'{result = false, message = "Malformed auction bid request"}),
-      gen_tcp:send(Socket, Reply),
-      handle_investor(Socket, Username)
+      loginManager:logout(Username)
   end.
 
-logout_investor(Socket, Username, Bin) ->
+logout_investor(ClientSocket, Username, ExchangeMap, Bin) ->
   io:fwrite('Investor logout request. ~p~n', [Username]),
   case messages:decode_msg(Bin, 'LogoutRequest') of
     {_, _, Username} ->
@@ -201,17 +232,17 @@ logout_investor(Socket, Username, Bin) ->
         ok ->
           io:fwrite('Logout ok.\n'),
           Reply = messages:encode_msg(#'Reply'{result = true, message = "Logout ok"}),
-          gen_tcp:send(Socket, Reply),
-          authenticate(Socket);
+          gen_tcp:send(ClientSocket, Reply),
+          authenticate(ClientSocket);
         _ ->
           io:fwrite('Logout not ok.\n'),
           Reply = messages:encode_msg(#'Reply'{result = false, message = "Logout not ok"}),
-          gen_tcp:send(Socket, Reply),
-          handle_investor(Socket, Username)
+          gen_tcp:send(ClientSocket, Reply),
+          handle_investor(ClientSocket, Username, ExchangeMap)
       end;
     _ ->
       io:fwrite('Logout not ok.\n'),
       Reply = messages:encode_msg(#'Reply'{result = false, message = "Logout not ok"}),
-      gen_tcp:send(Socket, Reply),
-      handle_investor(Socket, Username)
+      gen_tcp:send(ClientSocket, Reply),
+      handle_investor(ClientSocket, Username, ExchangeMap)
   end.
